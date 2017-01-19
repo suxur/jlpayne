36f60660b4684f4197eb42b8e11472d69bfd85ba
Joshua Payne <josh@agentimpress.me>
2017-01-18 20:52:18 -0600

    Finalize Basic Site

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,19 +11,6 @@ let mix = require('laravel-mix').mix;
  |
  */
 
-mix.js('src/js/app.js', 'public/js')
-   .sass('src/sass/app.sass', 'public/css/');
-
-// Full API
-// mix.js(src, output);
-// mix.extract(vendorLibs);
-// mix.sass(src, output);
-// mix.less(src, output);
-// mix.combine(files, destination);
-// mix.copy(from, to);
-// mix.minify(file);
-// mix.sourceMaps(); // Enable sourcemaps
-// mix.version(); // Enable versioning.
-// mix.disableNotifications();
-// mix.setPublicPath('path/to/public'); <-- Useful for Node apps.
-// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+mix
+    .js('src/js/app.js', 'public/js')
+    .sass('src/sass/app.sass', 'public/css/');
