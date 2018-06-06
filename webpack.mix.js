45bf7e0114906344cb0fbe16a1e1ccfd5b4f0b54
Joshua Payne <suxur@me.com>
2018-06-06 00:09:09 -0500

    Update print assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.sass', 'public/css')
-    .sass('resources/assets/sass/print.sass', 'public/css');
+    .sass('resources/assets/sass/print.sass', 'public/css')
+    .options({
+        processCssUrls: false
+    });
